f4ea7caa09015639b75479f0379baeb36e83bb36
hrj <harshad.rj@gmail.com>
2017-06-24 10:00:35 +0530

    added demo code

--- src/com/lavadip/smokhttp/App.java
+++ src/com/lavadip/smokhttp/App.java
@@ -1,21 +1,53 @@
 package com.lavadip.smokhttp;
 
-import java.io.FileWriter;
 import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 import java.security.Policy;
+import java.util.Arrays;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 
 public class App {
 
-	public static void main(String[] args) throws IOException {
-		Policy.setPolicy(LocalSecurityPolicy.getInstance());
-		System.setSecurityManager(new LocalSecurityManager());
+  public static void main(final String[] args) throws IOException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
+    Policy.setPolicy(LocalSecurityPolicy.getInstance());
+    System.setSecurityManager(new LocalSecurityManager());
 
-		System.out.println("Security manager installed.");
+    System.out.println("Security manager installed.");
 
-		System.out.println("The following file access should fail with a security exception.");
-		final FileWriter writer = new FileWriter("test-file-removeme.out");
-		writer.write("dummy text");
-		writer.close();
-	}
+    final TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(
+        TrustManagerFactory.getDefaultAlgorithm());
+    trustManagerFactory.init((KeyStore) null);
+    final TrustManager[] trustManagers = trustManagerFactory.getTrustManagers();
+    if (trustManagers.length != 1 || !(trustManagers[0] instanceof X509TrustManager)) {
+      throw new IllegalStateException("Unexpected default trust managers:" + Arrays.toString(trustManagers));
+    }
+    final X509TrustManager trustManager = (X509TrustManager) trustManagers[0];
+
+    final SSLContext sslContext = SSLContext.getInstance("TLS");
+    sslContext.init(null, new TrustManager[] { trustManager }, null);
+    final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+
+    System.out.println("Creating okhttp client");
+    final OkHttpClient client = new OkHttpClient.Builder()
+        .sslSocketFactory(sslSocketFactory, trustManager)
+        .build();
+
+    System.out.println("Creating okhttp request");
+    final Request request = new Request.Builder().url("https://stripe.com").build();
+    client.newCall(request);
+
+    System.out.println("All fine");
+  }
 
 }
--- src/com/lavadip/smokhttp/LocalSecurityPolicy.java
+++ src/com/lavadip/smokhttp/LocalSecurityPolicy.java
@@ -1,12 +1,21 @@
 package com.lavadip.smokhttp;
 
+import java.net.NetPermission;
+import java.security.CodeSource;
+import java.security.PermissionCollection;
+import java.security.Permissions;
 import java.security.Policy;
 
-public class LocalSecurityPolicy {
+public class LocalSecurityPolicy extends Policy {
 
 	public static Policy getInstance() {
-		// TODO Auto-generated method stub
-		return null;
+		return new LocalSecurityPolicy();
 	}
 
+	@Override
+	public PermissionCollection getPermissions(CodeSource codesource) {
+	  final PermissionCollection permissions = new Permissions();
+	  permissions.add(new NetPermission("getProxySelector"));
+	  return permissions;
+	}
 }
